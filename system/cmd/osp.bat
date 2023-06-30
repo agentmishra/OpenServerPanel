8e7bb8ff6918cbe27de3e5cfdad2cd05586ffa43
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-06-30 17:03:10 +0300

    Delete osp.bat

--- system/cmd/osp.bat
+++
@@ -1,2 +0,0 @@
-@if not exist "%~dp0..\..\bin\osp.bat" @echo: & @echo First you need to run the Open Server Panel! & @echo Type the command "ospanel" and press Enter... & @exit /b 1
-@"%~dp0..\..\bin\osp.bat" %*
