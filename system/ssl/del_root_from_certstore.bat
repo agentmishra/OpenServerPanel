0d62c9a895aeeec66b4e9fe11dde063216241133
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-07-03 18:20:43 +0300

    Update del_root_from_certstore.bat

--- system/ssl/del_root_from_certstore.bat
+++ system/ssl/del_root_from_certstore.bat
@@ -1,4 +1,4 @@
 @echo off
 if not exist "%~dp0..\..\data\ssl\root\cert.crt" exit /b 1
-certutil.exe -user -delstore "Root" "%~dp0..\..\data\ssl\root\cert.crt"
+certutil.exe -user -delstore "Root" "Open Server Panel"
 certutil.exe -urlcache * delete >nul 2>nul
