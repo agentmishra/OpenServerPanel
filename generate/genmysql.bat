6db37e392c74f15a292ea0dc60189360a7231f8f
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-08-11 22:52:44 +0300

    Update genmysql.bat

--- generate/genmysql.bat
+++ generate/genmysql.bat
@@ -18,18 +18,18 @@ del "%OSP_ROOT_DIR%modules\%1\*.ini" /q 2>nul
 call osp init %1 initdb
 call osp use Perl
 call osp add %1
-rd    "%OSP_ROOT_DIR%data\%1" /s /q 2>nul
-mkdir "%OSP_ROOT_DIR%data\%1"
-mkdir "%OSP_ROOT_DIR%generate\new_data\%1"
+rd    "%OSP_ROOT_DIR%data\%1\default" /s /q 2>nul
+mkdir "%OSP_ROOT_DIR%data\%1\default"
+mkdir "%OSP_ROOT_DIR%generate\new_data\%1\default"
 cd /d "%OSP_ROOT_DIR%modules\%1"
 copy my.ini my-default.ini
 copy my.ini my_print_defaults.ini
-if not "%1"=="MySQL-5.7" if not "%1"=="MySQL-8.0" call perl scripts\mysql_install_db.pl --basedir="%OSP_ROOT_DIR%modules\%1" --datadir="%OSP_ROOT_DIR%data\%1" --skip-name-resolve --windows --verbose
+if not "%1"=="MySQL-5.7" if not "%1"=="MySQL-8.0" call perl scripts\mysql_install_db.pl --basedir="%OSP_ROOT_DIR%modules\%1" --datadir="%OSP_ROOT_DIR%data\%1\default" --skip-name-resolve --windows --verbose
 if not "%1"=="MySQL-5.5" if not "%1"=="MySQL-5.6" start "" "%OSP_ROOT_DIR%modules\%1\bin\mysqld.exe" --defaults-file="%OSP_ROOT_DIR%modules\%1\my.ini" --initialize-insecure --console --standalone
 timeout /t 3 /nobreak > nul
 del "%OSP_ROOT_DIR%modules\%1\*.ini" /q 2>nul
-if not "%1"=="MySQL-5.5" copy "%OSP_ROOT_DIR%generate\setup\private_key.pem" "%OSP_ROOT_DIR%data\%1\private_key.pem"
-if not "%1"=="MySQL-5.5" copy "%OSP_ROOT_DIR%generate\setup\public_key.pem" "%OSP_ROOT_DIR%data\%1\public_key.pem"
+if not "%1"=="MySQL-5.5" copy "%OSP_ROOT_DIR%generate\setup\private_key.pem" "%OSP_ROOT_DIR%data\%1\default\private_key.pem"
+if not "%1"=="MySQL-5.5" copy "%OSP_ROOT_DIR%generate\setup\public_key.pem" "%OSP_ROOT_DIR%data\%1\default\public_key.pem"
 call osp on %1
 timeout /t 5 /nobreak > nul
 if "%1"=="MySQL-5.7" call mysql --protocol=PIPE --socket=%1 --host="" -u root mysql -e "INSTALL PLUGIN mysqlx SONAME 'mysqlx.dll';"
@@ -42,9 +42,9 @@ timeout /t 5 /nobreak > nul
 call mysql --force --protocol=PIPE --socket=%1 --host="" -u root mysql < "%OSP_ROOT_DIR%generate\setup\install.sql"
 call osp off %1
 timeout /t 3 /nobreak > nul
-del "%OSP_ROOT_DIR%data\%1\*.ini" /q 2>nul
-del "%OSP_ROOT_DIR%data\%1\*.err" /q 2>nul
-robocopy "%OSP_ROOT_DIR%data\%1" "%OSP_ROOT_DIR%generate\new_data\%1" /UNICODE /DCOPY:DAT /COPY:DAT /TIMFIX /MIR /J /ETA /IM /MT:32 /R:3 /W:3 >nul 2>nul
+del "%OSP_ROOT_DIR%data\%1\default\*.ini" /q 2>nul
+del "%OSP_ROOT_DIR%data\%1\default\*.err" /q 2>nul
+robocopy "%OSP_ROOT_DIR%data\%1\default" "%OSP_ROOT_DIR%generate\new_data\%1\default" /UNICODE /DCOPY:DAT /COPY:DAT /TIMFIX /MIR /J /ETA /IM /MT:32 /R:3 /W:3 >nul 2>nul
 exit /b 0
 :end
 echo on
