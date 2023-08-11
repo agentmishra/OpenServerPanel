a545adb468219af249bafd3583c4d885db376d12
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-08-11 22:52:29 +0300

    Update genmariadb1.bat

--- generate/genmariadb1.bat
+++ generate/genmariadb1.bat
@@ -18,13 +18,13 @@ call osp off %1
 del "%OSP_ROOT_DIR%modules\%1\*.ini" /q 2>nul
 call osp init %1 initdb
 call osp use %1
-rd    "%OSP_ROOT_DIR%data\%1" /s /q 2>nul
-mkdir "%OSP_ROOT_DIR%data\%1"
-mkdir "%OSP_ROOT_DIR%generate\new_data\%1"
+rd    "%OSP_ROOT_DIR%data\%1\default" /s /q 2>nul
+mkdir "%OSP_ROOT_DIR%data\%1\default"
+mkdir "%OSP_ROOT_DIR%generate\new_data\%1\default"
 cd /d "%OSP_ROOT_DIR%modules\%1"
 copy my.ini my-default.ini
 copy my.ini my_print_defaults.ini
-call mysql_install_db --datadir="%OSP_ROOT_DIR%data\%1" --allow-remote-root-access -o
+call mysql_install_db --datadir="%OSP_ROOT_DIR%data\%1\default" --allow-remote-root-access -o
 timeout /t 3 /nobreak > nul
 del "%OSP_ROOT_DIR%modules\%1\*.ini" /q 2>nul
 call osp on %1
@@ -37,9 +37,9 @@ timeout /t 5 /nobreak > nul
 call mysql --force --protocol=PIPE --socket=%1 --host="" -u root mysql < "%OSP_ROOT_DIR%generate\setup\install.sql"
 call osp off %1
 timeout /t 3 /nobreak > nul
-del "%OSP_ROOT_DIR%data\%1\*.ini" /q 2>nul
-del "%OSP_ROOT_DIR%data\%1\*.err" /q 2>nul
-robocopy "%OSP_ROOT_DIR%data\%1" "%OSP_ROOT_DIR%generate\new_data\%1" /UNICODE /DCOPY:DAT /COPY:DAT /TIMFIX /MIR /J /ETA /IM /MT:32 /R:3 /W:3 >nul 2>nul
+del "%OSP_ROOT_DIR%data\%1\default\*.ini" /q 2>nul
+del "%OSP_ROOT_DIR%data\%1\default\*.err" /q 2>nul
+robocopy "%OSP_ROOT_DIR%data\%1\default" "%OSP_ROOT_DIR%generate\new_data\%1\default" /UNICODE /DCOPY:DAT /COPY:DAT /TIMFIX /MIR /J /ETA /IM /MT:32 /R:3 /W:3 >nul 2>nul
 exit /b 0
 :end
 echo on
