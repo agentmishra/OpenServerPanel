4f98de90eac29ea3ed9b918791d8a96b1c0daeab
OSPanel <93676052+OSPanel@users.noreply.github.com>
2023-12-17 01:35:47 +0300

    Update genhelp.bat

--- generate/genhelp.bat
+++ generate/genhelp.bat
@@ -62,39 +62,39 @@ call osp use %1
 call osp add perl
 set LC_MESSAGES=English
 cd /d %OSP_ROOT_DIR%modules\%1
-rd "%OSP_ROOT_DIR%generate\%1\ospanel_data\help" /s /q 2>nul
-mkdir "%OSP_ROOT_DIR%generate\%1\ospanel_data\help" 2>nul
+rd "%OSP_ROOT_DIR%generate\help\%1\ospanel_data\help" /s /q 2>nul
+mkdir "%OSP_ROOT_DIR%generate\help\%1\ospanel_data\help" 2>nul
 if not exist named.exe if not exist bin\isolationtester.exe call :reader %1 %2
 if exist named.exe if not exist bin\isolationtester.exe call :reader2 %1 %2
 if exist bin\isolationtester.exe call :reader3 %1 %2
-forfiles /S /M *.pl /C "cmd /c call perl @file --help >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\@file.txt 2>&1"
-if exist bin\ibd2sdi.exe call ibd2sdi.exe --help >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\ibd2sdi.exe.txt 2>&1
-if exist bin\mysqld.exe call mysqld.exe --verbose --help >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\mysqld.exe.txt 2>&1
-if exist bin\mariadbd.exe call mariadbd.exe --verbose --help >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\mariadbd.exe.txt 2>&1
-if exist apache\bin\sqlite3.exe call sqlite3.exe -help >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\sqlite3.exe.txt 2>&1
-if exist apache\bin\openssl.exe call openssl.exe help >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\openssl.exe.txt 2>&1
-if exist named.exe call named.exe -f help >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\named.exe.txt 2>&1
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\mysql_secure_installation.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\mysql_tzinfo_to_sql.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\echo.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\deplister.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\redis-check-aof.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\redis-check-dump.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\redis-check-rdb.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\stackbuilder.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\sizes.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\testapp.exe.txt 2>nul
-del /Q  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\arpaname.exe.txt 2>nul
+forfiles /S /M *.pl /C "cmd /c call perl @file --help >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\@file.txt 2>&1"
+if exist bin\ibd2sdi.exe call ibd2sdi.exe --help >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\ibd2sdi.exe.txt 2>&1
+if exist bin\mysqld.exe call mysqld.exe --verbose --help >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\mysqld.exe.txt 2>&1
+if exist bin\mariadbd.exe call mariadbd.exe --verbose --help >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\mariadbd.exe.txt 2>&1
+if exist apache\bin\sqlite3.exe call sqlite3.exe -help >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\sqlite3.exe.txt 2>&1
+if exist apache\bin\openssl.exe call openssl.exe help >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\openssl.exe.txt 2>&1
+if exist named.exe call named.exe -f help >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\named.exe.txt 2>&1
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\mysql_secure_installation.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\mysql_tzinfo_to_sql.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\echo.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\deplister.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\redis-check-aof.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\redis-check-dump.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\redis-check-rdb.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\stackbuilder.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\sizes.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\testapp.exe.txt 2>nul
+del /Q  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\arpaname.exe.txt 2>nul
 cd /d %~dp0
 exit /b 0
 :reader
-forfiles /S /M *.exe /C "cmd /c if /i not @fname==\"gswin64\" if /i not @fname==\"mariadb-upgrade-wizard\" if /i not @fname==\"mysql_upgrade_wizard\" if /i not @fname==\"phpdbg\" call @file %2 >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\@file.txt 2>&1"
+forfiles /S /M *.exe /C "cmd /c if /i not @fname==\"gswin64\" if /i not @fname==\"mariadb-upgrade-wizard\" if /i not @fname==\"mysql_upgrade_wizard\" if /i not @fname==\"phpdbg\" call @file %2 >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\@file.txt 2>&1"
 exit /b 0
 :reader2
-forfiles /S /M *.exe /C "cmd /c if /i not @fname==\"nslookup\" call @file %2 >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\@file.txt 2>&1"
+forfiles /S /M *.exe /C "cmd /c if /i not @fname==\"nslookup\" call @file %2 >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\@file.txt 2>&1"
 exit /b 0
 :reader3
-forfiles /S /M *.exe /C "cmd /c if /i not @fname==\"pgAdmin3\" if /i not @fname==\"isolationtester\" if /i not @fname==\"stackbuilder\" call @file %2 >  %OSP_ROOT_DIR%generate\%1\ospanel_data\help\@file.txt 2>&1"
+forfiles /S /M *.exe /C "cmd /c if /i not @fname==\"pgAdmin3\" if /i not @fname==\"isolationtester\" if /i not @fname==\"stackbuilder\" call @file %2 >  %OSP_ROOT_DIR%generate\help\%1\ospanel_data\help\@file.txt 2>&1"
 exit /b 0
 :end
 call osp use perl
